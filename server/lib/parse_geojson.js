db7dc38fa4f5085a179f9160254e286a0f276a70
Michael Kreil <github@michael-kreil.de>
2015-05-09 06:46:50 +0200

    parsers

---
+++ server/lib/parse_geojson.js
@@ -0,0 +1,92 @@
+
+var fs = require('fs');
+var path = require('path');
+var util = require('util');
+var c = require('../config.js');
+
+module.exports = function (entry) {
+	var filename = path.resolve(c.source_folder, entry.filename);
+
+	var data = fs.readFileSync(filename, 'utf8');
+	data = JSON.parse(data);
+	
+	var geo = [];
+	var properties = {};
+	var index = 0;
+	data.features.forEach(function (feature) {
+		if (feature.type != 'Feature') throw new Error();
+		geo[index] = parseGeometry(feature.geometry);
+
+		Object.keys(feature.properties).forEach(function (key) {
+			if (!properties[key]) properties[key] = [];
+			properties[key][index] = feature.properties[key];
+		})
+		index++;
+	});
+	
+	var geofilename = entry.name;
+	fs.writeFileSync(path.resolve(c.data_folder, 'geo', geofilename+'.json'), JSON.stringify(geo), 'utf8');
+
+	var result = {};
+	entry.properties.forEach(function (property) {
+		var values = properties[property.key];
+		if (!values) throw Error();
+
+		delete properties[property.key];
+
+		if (property.ignore) return;
+
+		if (property.parser) values = values.map(function (value) {
+			if (value === undefined) return undefined;
+			return property.parser(value);
+		})
+
+		var propfilename = entry.name+'_'+(property.name || property.key);
+		fs.writeFileSync(path.resolve(c.data_folder, 'property', propfilename+'.json'), JSON.stringify(values), 'utf8');
+
+		result[propfilename] = {
+			geo:geofilename,
+			property:propfilename,
+			nameGeo: entry.title || entry.name,
+			nameProp: property.title || property.name || property.key
+		}
+	})
+
+	Object.keys(properties).forEach(function (key) {
+		var values = properties[key];
+		console.error('Missing "'+key+'"');
+		values.sort();
+		console.log(JSON.stringify(values));
+		process.exit();
+	})
+
+	return result;
+}
+
+function parseGeometry(geometry) {
+	switch (geometry.type) {
+		case 'LineString': return { t:'l', c:diffEncoding(geometry.coordinates.map(parseCoordinate)) };
+		default: throw new Error();
+	}
+}
+
+function parseCoordinate(coord) {
+	return [
+		Math.round((coord[0]-10)*1000000),
+		Math.round((coord[1]-51)*1000000)
+	]
+}
+
+function diffEncoding(values) {
+	var lastCoord;
+	return values.map(function (coord, index) {
+		var newCoord;
+		if (index == 0) {
+			newCoord = coord;
+		} else {
+			newCoord = [coord[0] - lastCoord[0], coord[1] - lastCoord[1]];
+		}
+		lastCoord = coord;
+		return newCoord;
+	})
+}
---
+++ server/parse.js
@@ -0,0 +1,69 @@
+
+var sources = [
+	{
+		type: 'geojson',
+		name: 'streckennetz',
+		title: 'Streckennetz',
+		filename: 'map/Streckennetz_WGS84.geojson',
+		properties: [
+			{key:'mifcode', ignore:true},
+			{key:'strecke_nr'},
+			{key:'richtung', parser:parseFloat},
+			{key:'laenge'},
+			{key:'von_km'},
+			{key:'bis_km'},
+			{key:'elektrifizierung'},
+			{key:'bahnnutzung'},
+			{key:'geschwindigkeit',
+				parser:function (value) {
+					switch(value) {
+						case null: return 0;
+						case 'bis 50 km/h': return 50;
+						case 'ab 50 bis 100 km/h': return 100;
+						case 'ab 100 bis 120 km/h': return 120;
+						case 'ab 120 bis 160 km/h': return 160;
+						case 'ab 160 bis 200 km/h': return 200;
+						case 'ab 200 bis 250 km/h': return 250;
+						case 'ab 250 bis 280 km/h': return 280;
+						case 'ab 280 bis 300 km/h': return 300;
+						default: throw Error();
+					}
+				}
+			},
+			{key:'strecke_kurzn'},
+			{key:'gleisanzahl',
+				parser:function (value) {
+					switch(value) {
+						case 'eingleisig': return 1;
+						case 'zweigleisig': return 2;
+						default: throw Error();
+					}
+				}
+			},
+			{key:'bahnart'},
+			{key:'kmspru_typ_anf'},
+			{key:'kmspru_typ_end'}
+		]
+	}
+]
+
+var fs = require('fs');
+var path = require('path');
+var c = require('./config.js');
+var parseGeoJSON = require('./lib/parse_geojson.js');
+
+var result = {};
+
+sources.forEach(function (entry) {
+	switch (entry.type) {
+		case 'geojson':
+			parseGeoJSON(entry)
+		break;
+		default:
+			throw new Error('Unknown type')
+	}
+})
+
+fs.writeFileSync(path.resolve(c.data_folder, 'data.json'), JSON.stringify(result), 'utf8')
+
+
