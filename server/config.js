5dd41726652e7c839b0f10ca3148c22798392da2
Michael Kreil <github@michael-kreil.de>
2015-05-09 06:46:42 +0200

    basic config

---
+++ server/config.js
@@ -0,0 +1,9 @@
+var path = require('path');
+
+var config = {
+	source_folder: path.resolve(__dirname, '../data'),
+	data_folder: path.resolve(__dirname, '../web/data')
+}
+
+
+module.exports = config;
